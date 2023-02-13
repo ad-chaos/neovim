e9bd495578821788b75e8e5fa36648b2dec0e3b0
ad_chaos <santoshrajpurohit89@gmail.com>
2023-02-13 23:26:28 +0530

    make quote text objects intuitive

--- src/nvim/textobject.c
+++ src/nvim/textobject.c
@@ -1449,34 +1449,6 @@ static int find_next_quote(char *line, int col, int quotechar, char *escape)
   return col;
 }
 
-/// Search backwards in "line" from column "col_start" to find "quotechar".
-/// Quote character escaped by one of the characters in "escape" is not counted
-/// as a quote.
-///
-/// @param escape  escape characters, can be NULL
-///
-/// @return        the found column or zero.
-static int find_prev_quote(char *line, int col_start, int quotechar, char *escape)
-{
-  while (col_start > 0) {
-    col_start--;
-    col_start -= utf_head_off(line, line + col_start);
-    int n = 0;
-    if (escape != NULL) {
-      while (col_start - n > 0 && vim_strchr(escape,
-                                             (uint8_t)line[col_start - n - 1]) != NULL) {
-        n++;
-      }
-    }
-    if (n & 1) {
-      col_start -= n;           // uneven number of escape chars, skip it
-    } else if ((uint8_t)line[col_start] == quotechar) {
-      break;
-    }
-  }
-  return col_start;
-}
-
 /// Find quote under the cursor, cursor at end.
 ///
 /// @param include    true == include quote char
@@ -1494,8 +1466,6 @@ bool current_quote(oparg_T *oap, long count, bool include, int quotechar)
   bool vis_bef_curs = false;            // Visual starts before cursor
   bool did_exclusive_adj = false;       // adjusted pos for 'selection'
   bool inside_quotes = false;           // Looks like "i'" done before
-  bool selected_quote = false;          // Has quote inside selection
-  int i;
   bool restore_vis_bef = false;         // restore VIsual on abort
 
   // When 'selection' is "exclusive" move the cursor to where it would be
@@ -1533,113 +1503,65 @@ bool current_quote(oparg_T *oap, long count, bool include, int quotechar)
   if (!vis_empty) {
     // Check if the existing selection exactly spans the text inside
     // quotes.
-    if (vis_bef_curs) {
-      inside_quotes = VIsual.col > 0
-                      && (uint8_t)line[VIsual.col - 1] == quotechar
-                      && line[curwin->w_cursor.col] != NUL
-                      && (uint8_t)line[curwin->w_cursor.col + 1] == quotechar;
-      i = VIsual.col;
-      col_end = curwin->w_cursor.col;
-    } else {
-      inside_quotes = curwin->w_cursor.col > 0
-                      && (uint8_t)line[curwin->w_cursor.col - 1] == quotechar
-                      && line[VIsual.col] != NUL
-                      && (uint8_t)line[VIsual.col + 1] == quotechar;
-      i = curwin->w_cursor.col;
-      col_end = VIsual.col;
-    }
-
-    // Find out if we have a quote in the selection.
-    while (i <= col_end) {
-      // check for going over the end of the line, which can happen if
-      // the line was changed after the Visual area was selected.
-      if (line[i] == NUL) {
-        break;
-      }
-      if ((uint8_t)line[i++] == quotechar) {
-        selected_quote = true;
-        break;
-      }
-    }
+    col_start = vis_bef_curs ? VIsual.col : curwin->w_cursor.col;
+    col_end = vis_bef_curs ? curwin->w_cursor.col : VIsual.col;
+    inside_quotes = col_start > 0
+                    && (uint8_t)line[col_start - 1] == quotechar
+                    && line[col_end] != NUL
+                    && (uint8_t)line[col_end + 1] == quotechar;
   }
 
-  if (!vis_empty && (uint8_t)line[col_start] == quotechar) {
-    // Already selecting something and on a quote character.  Find the
-    // next quoted string.
-    if (vis_bef_curs) {
-      // Assume we are on a closing quote: move to after the next
-      // opening quote.
-      col_start = find_next_quote(line, col_start + 1, quotechar, NULL);
-      if (col_start < 0) {
-        goto abort_search;
-      }
-      col_end = find_next_quote(line, col_start + 1, quotechar, curbuf->b_p_qe);
-      if (col_end < 0) {
-        // We were on a starting quote perhaps?
-        col_end = col_start;
-        col_start = curwin->w_cursor.col;
-      }
-    } else {
-      col_end = find_prev_quote(line, col_start, quotechar, NULL);
-      if ((uint8_t)line[col_end] != quotechar) {
-        goto abort_search;
-      }
-      col_start = find_prev_quote(line, col_end, quotechar, curbuf->b_p_qe);
-      if ((uint8_t)line[col_start] != quotechar) {
-        // We were on an ending quote perhaps?
-        col_start = col_end;
-        col_end = curwin->w_cursor.col;
-      }
-    }
-  } else if ((uint8_t)line[col_start] == quotechar || !vis_empty) {
-    int first_col = col_start;
+  int cursor_col = curwin->w_cursor.col;
+  int old_col_start = -1;
+  int old_col_end = -1;
 
-    if (!vis_empty) {
-      if (vis_bef_curs) {
-        first_col = find_next_quote(line, col_start, quotechar, NULL);
-      } else {
-        first_col = find_prev_quote(line, col_start, quotechar, NULL);
-      }
+  // The cursor is on a quote, we don't know if it's the opening or
+  // closing quote.  Search from the start of the line to find out.
+  col_start = 0;
+  col_end = -1;
+  for (;;) {
+    // Find open quote character.
+    col_start = find_next_quote(line, col_start, quotechar, NULL);
+    if (col_start < 0) {
+      break;
     }
-    // The cursor is on a quote, we don't know if it's the opening or
-    // closing quote.  Search from the start of the line to find out.
-    // Also do this when there is a Visual area, a' may leave the cursor
-    // in between two strings.
-    col_start = 0;
-    for (;;) {
-      // Find open quote character.
-      col_start = find_next_quote(line, col_start, quotechar, NULL);
-      if (col_start < 0 || col_start > first_col) {
-        goto abort_search;
-      }
-      // Find close quote character.
-      col_end = find_next_quote(line, col_start + 1, quotechar, curbuf->b_p_qe);
-      if (col_end < 0) {
-        goto abort_search;
-      }
-      // If is cursor between start and end quote character, it is
-      // target text object.
-      if (col_start <= first_col && first_col <= col_end) {
-        break;
-      }
-      col_start = col_end + 1;
-    }
-  } else {
-    // Search backward for a starting quote.
-    col_start = find_prev_quote(line, col_start, quotechar, curbuf->b_p_qe);
-    if ((uint8_t)line[col_start] != quotechar) {
-      // No quote before the cursor, look after the cursor.
-      col_start = find_next_quote(line, col_start, quotechar, NULL);
-      if (col_start < 0) {
-        goto abort_search;
-      }
-    }
-
     // Find close quote character.
     col_end = find_next_quote(line, col_start + 1, quotechar, curbuf->b_p_qe);
     if (col_end < 0) {
-      goto abort_search;
+      if (vis_bef_curs || vis_empty) {
+        old_col_start = old_col_end;
+        old_col_end = col_start;
+      }
+      break;
     }
+    // If cursor is before the end quote character, it is
+    // target text object.
+    if (cursor_col < col_end && --count <= 0) {
+      break;
+    }
+
+    old_col_start = col_start;
+    old_col_end = col_end;
+    col_start = col_end + 1;
+  }
+
+  if (old_col_start >= 0 && old_col_end > 0
+      && (col_start <= 0 || col_end < 0 || (!vis_bef_curs && !vis_empty && !inside_quotes))) {
+    col_start = old_col_start;
+    col_end = old_col_end;
+  } else if (col_end < 0 && old_col_start <= 0) {
+    if (VIsual_active && *p_sel == 'e') {
+      if (did_exclusive_adj) {
+        inc_cursor();
+      }
+      if (restore_vis_bef) {
+        pos_T t = curwin->w_cursor;
+
+        curwin->w_cursor = VIsual;
+        VIsual = t;
+      }
+    }
+    return false;
   }
 
   // When "include" is true, include spaces after closing quote or before
@@ -1658,24 +1580,15 @@ bool current_quote(oparg_T *oap, long count, bool include, int quotechar)
 
   // Set start position.  After vi" another i" must include the ".
   // For v2i" include the quotes.
-  if (!include && count < 2 && (vis_empty || !inside_quotes)) {
+  if (!include && (vis_empty || !inside_quotes)) {
     col_start++;
   }
+
   curwin->w_cursor.col = col_start;
   if (VIsual_active) {
-    // Set the start of the Visual area when the Visual area was empty, we
-    // were just inside quotes or the Visual area didn't start at a quote
-    // and didn't include a quote.
-    if (vis_empty
-        || (vis_bef_curs
-            && !selected_quote
-            && (inside_quotes
-                || ((uint8_t)line[VIsual.col] != quotechar
-                    && (VIsual.col == 0
-                        || (uint8_t)line[VIsual.col - 1] != quotechar))))) {
-      VIsual = curwin->w_cursor;
-      redraw_curbuf_later(UPD_INVERTED);
-    }
+    // Find out if we have a quote in the selection.
+    VIsual = curwin->w_cursor;
+    redraw_curbuf_later(UPD_INVERTED);
   } else {
     oap->start = curwin->w_cursor;
     oap->motion_type = kMTCharWise;
@@ -1683,9 +1596,7 @@ bool current_quote(oparg_T *oap, long count, bool include, int quotechar)
 
   // Set end position.
   curwin->w_cursor.col = col_end;
-  if ((include || count > 1
-       // After vi" another i" must include the ".
-       || (!vis_empty && inside_quotes)) && inc_cursor() == 2) {
+  if ((include || (!vis_empty && inside_quotes)) && inc_cursor() == 2) {
     inclusive = true;
   }
   if (VIsual_active) {
@@ -1698,15 +1609,9 @@ bool current_quote(oparg_T *oap, long count, bool include, int quotechar)
       // Cursor is at start of Visual area.  Set the end of the Visual
       // area when it was just inside quotes or it didn't end at a
       // quote.
-      if (inside_quotes
-          || (!selected_quote
-              && (uint8_t)line[VIsual.col] != quotechar
-              && (line[VIsual.col] == NUL
-                  || (uint8_t)line[VIsual.col + 1] != quotechar))) {
         dec_cursor();
         VIsual = curwin->w_cursor;
-      }
-      curwin->w_cursor.col = col_start;
+        curwin->w_cursor.col = col_start;
     }
     if (VIsual_mode == 'V') {
       VIsual_mode = 'v';
@@ -1718,18 +1623,4 @@ bool current_quote(oparg_T *oap, long count, bool include, int quotechar)
   }
 
   return true;
-
-abort_search:
-  if (VIsual_active && *p_sel == 'e') {
-    if (did_exclusive_adj) {
-      inc_cursor();
-    }
-    if (restore_vis_bef) {
-      pos_T t = curwin->w_cursor;
-
-      curwin->w_cursor = VIsual;
-      VIsual = t;
-    }
-  }
-  return false;
 }
